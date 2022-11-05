295d8bd7b13be546de78c9ea9ea6fd805819e7f0
Juliana <juliana.batista@unifesspa.edu.br>
2022-11-05 19:25:55 -0300

    Add section podcasts

--- js/main.js
+++ js/main.js
@@ -195,3 +195,13 @@
     });
 
 })(jQuery);
+$(".video-play").on('click', function (e) {
+    e.preventDefault();
+    var vidWrap = $(this).parent(),
+        iframe = vidWrap.find('.video iframe'),
+        iframeSrc = iframe.attr('src'),
+        iframePlay = iframeSrc += "?autoplay=1";
+    vidWrap.children('.video-thumbnail').fadeOut();
+    vidWrap.children('.video-play').fadeOut();
+    vidWrap.find('.video iframe').attr('src', iframePlay);
+});
